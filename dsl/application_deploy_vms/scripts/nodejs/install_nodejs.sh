f48c0ce3a1b5ac56f35527b1a0b5c15fa5d8d871
nomad <unknown>
2022-08-17 10:01:06 -0600

    Updated Node.js install

--- dsl/application_deploy_vms/scripts/nodejs/install_nodejs.sh
+++ dsl/application_deploy_vms/scripts/nodejs/install_nodejs.sh
@@ -1,17 +1,14 @@
-echo "Install Epel Repositories"
-sudo yum install -y epel-release
-sudo yum install -y nodejs
-
-echo "Install Node.js"
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-sudo yum install -y nodejs
-
-echo "Configure firewall"
-sudo firewall-cmd --permanent --zone=public --add-port=3000/tcp
-sudo firewall-cmd --permanent --add-service='ssh'
-sudo firewall-cmd --reload
-
 sudo yum install -y wget
+curl "https://nodejs.org/dist/v17.9.1/node-v17.9.1-linux-x64.tar.gz" > "$HOME/nodejs.tar.gz"
+
+tar xvf nodejs.tar.gz
+sudo mkdir /usr/local/bin/nodejs
+sudo mv node-v17.9.1-linux-x64/* /usr/local/bin/nodejs/
+ls -al node-v17.9.1-linux-x64
+rm -rf node-v17.9.1-linux-x64
+
+export PATH=$PATH:/usr/local/bin/nodejs/bin
+echo "PATH=$PATH:/usr/local/bin/nodejs/bin" >> ~/.bashrc
 
 echo""
 echo "Node Version"
@@ -19,3 +16,4 @@ node --version
 echo""
 echo "NPM Version"
 npm --version
+
