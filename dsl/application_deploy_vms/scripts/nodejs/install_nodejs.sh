e74418a809263d38478fd94eb244cb0090643ff3
nomad <unknown>
2024-07-09 10:09:28 -0600

    Updated

--- dsl/application_deploy_vms/scripts/nodejs/install_nodejs.sh
+++ dsl/application_deploy_vms/scripts/nodejs/install_nodejs.sh
@@ -1,3 +1,15 @@
+sudo mv /etc/yum.repos.d/CentOS-Base.repo /tmp/CentOS-Base.repo.bak
+sudo bash -c 'cat << \EOF > /etc/yum.repos.d/CentOS-Base.repo
+[base]
+name=CentOS-$releasever - Base
+#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra
+#baseurl=http://mirror.centos.org/centos/$releasever/os/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/7.9.2009/os/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF'
+sudo yum clean all
+
 sudo yum install -y wget
 curl "https://nodejs.org/dist/v17.9.1/node-v17.9.1-linux-x64.tar.gz" > "$HOME/nodejs.tar.gz"
 
