bace8f79c0616413965cdebde29da020eeccbbe3
nomad <unknown>
2024-07-16 16:50:47 -0600

    Updated

--- dsl/application_deploy_vms/scripts/mongodb/install_mongodb.sh
+++ dsl/application_deploy_vms/scripts/mongodb/install_mongodb.sh
@@ -1,6 +1,8 @@
 sudo apt-get update -y
 sudo apt-get install -y mongodb-org
 
+sudo sed -i 's/  bindIp: 127.0.0.1/  bindIpAll: true/' /etc/mongod.conf 
+
 
 echo "Enable MongoDB Service"
 sudo systemctl enable mongod
